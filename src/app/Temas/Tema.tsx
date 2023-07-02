c48368f213a7b34289a20e47bd85f186710802e9
Gabriel <g.holandatry@gmail.com>
2023-07-01 21:04:16 -0300

    Adicionando Header, Conteudo e Footer inicial

---
+++ src/app/Temas/Tema.tsx
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+    
+const theme = createTheme(
+  {
+    typography: {
+      fontFamily: [
+        'bernard-mt'
+      ].join(','),
+    },
+  },
+);
+
+export default theme;
---
+++ src/app/componentes/Imagem.tsx
@@ -0,0 +1,5 @@
+import Image from 'next/image'
+ 
+export function Avatar() {
+  return <Image src="/logo.png" alt="logo"/>
+}
