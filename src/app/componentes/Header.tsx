642a776f541e1a30f87aa31c9df74e29a86f0f50
Gabriel <g.holandatry@gmail.com>
2023-08-30 10:31:43 -0300

    melhorando fontes e logos

--- src/app/componentes/Header.tsx
+++ src/app/componentes/Header.tsx
@@ -1,10 +1,15 @@
-import { Alumni_Sans } from 'next/font/google'
+import { Alumni_Sans, Montserrat } from 'next/font/google'
 
 
 const oswald = Alumni_Sans({
   subsets: ['latin'],
   weight: [ '300', '400']
 })
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
 const links = [
     { name: 'Distribuidores', href: '#' },
@@ -16,18 +21,18 @@ const links = [
   
 export default function Header() {
     return (
-        <div style={{ backgroundColor: '#C26641' }} className="relative isolate overflow-hidden py-5 sm:py-5">
+        <div style={{ backgroundColor: '#C26641' }} className="relative isolate overflow-hidden">
           <div className=" grid grid-cols-1 md:grid-cols-4 lg:grid-cols-6">
-            <div className="max-w-xs">
+            <div className="max-w-xs p-7">
               <img
-                src='logov3-positivo@4x.png'
+                src='logov3-positivo4x.png'
                 alt=""
                 className=""
                 
               />
             </div>
             <div className="ml-10 col-span-3 mt-10 max-w-2xl lg:mx-0 lg:max-w-none" style={{ textShadow: '1px 1px 2px black' }}>
-              <div className={"text-bottom font-semibold text-white sm:grid-cols-2 md:flex " + oswald.className}>
+              <div className={"text-bottom font-semibold text-white sm:grid-cols-2 md:flex " + montserrat.className}>
                 {links.map((link) => (
                   <a key={link.name} href={link.href} className='align-bottom mt-16 text-[16px] md:text-[22px] lg:text-[28px] px-3'>
                     {link.name}
